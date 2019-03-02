b6673553cf346f5883e688838e8751a4bde41f92
frolovakate13 <frolova.kate.13@gmail.com>
2019-03-02 16:59:40 +0500

    Изменения верстки страницы About

--- about.php
+++ about.php
@@ -48,7 +48,6 @@ if (isset($data['do_login'])) {
     </div>
 </div>
 
-<form>
 <div id="about">
     <div class="container marketing">
 
@@ -56,22 +55,32 @@ if (isset($data['do_login'])) {
         <div class="row">
             <div class="col-lg-4">
                 <img class="img-circle" src="img/bg0.jpg" width="140" height="140">
-                <h2>Heading</h2>
+                <h2>Тут должна быть инфа о сайте</h2>
                 <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
                 <img class="img-circle" src="img/bg0.jpg" width="140" height="140">
-                <h2>Heading</h2>
+                <h2>И тут</h2>
                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
                 <img class="img-circle" src="img/bg0.jpg" width="140" height="140">
-                <h2>Heading</h2>
+                <h2>И тут</h2>
                 <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
             </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
+        </div>
+
+        <h2>Что мы использовали?</h2>
+        <p> Тут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологии
+            Тут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологии
+            Тут много текста про технологииТут много текста про технологииТут много текста про технологии
+            Тут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологии
+            Тут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологииТут много текста про технологии
+            Тут много текста про технологииТут много текста про технологииТут много текста про технологии
+        </p>
+
+    </div>
 </div>
-</div>>
 
 </form>
 <div id="footer">
